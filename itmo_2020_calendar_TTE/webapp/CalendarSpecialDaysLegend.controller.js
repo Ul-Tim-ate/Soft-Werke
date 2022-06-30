96112853593e3b5eda81c365cd96e6eba91d5f3d
Ul-Tim-ate <68898246+Ul-Tim-ate@users.noreply.github.com>
2022-06-30 03:35:27 +0300

    Update CalendarSpecialDaysLegend.controller.js

--- itmo_2020_calendar_TTE/webapp/CalendarSpecialDaysLegend.controller.js
+++ itmo_2020_calendar_TTE/webapp/CalendarSpecialDaysLegend.controller.js
@@ -3,67 +3,52 @@ sap.ui.define([
 		'sap/ui/unified/CalendarLegendItem',
 		'sap/ui/unified/DateTypeRange',
 		'sap/ui/unified/library',
-		"sap/ui/model/json/JSONModel"
-	], function(Controller, CalendarLegendItem, DateTypeRange, unifiedLibrary, JSONModel) {
+	], function(Controller, CalendarLegendItem, DateTypeRange, unifiedLibrary) {
 	"use strict";
 
 	var CalendarDayType = unifiedLibrary.CalendarDayType;
-	var month, data;
+	var data;
 	return Controller.extend("sap.ui.unified.sample.CalendarSpecialDaysLegend.CalendarSpecialDaysLegend", {
-		onInit : function () {
-			var oModel = new JSONModel();
-			oModel.loadData("days.json");
-			data = oModel;
-    	},
-
-		handleShowSpecialDays: function(oEvent) {
-			var oCal = this.byId("calendar"),
+		
+		 onInit : function () {
+			this.byId('calendar').displayDate(
+        		new Date(new Date().getFullYear().toString())
+        	);
+	        var oSpecialDates = this.getOwnerComponent().getModel('calendarSpecialDates').getData(),
+        		oHolidays = oSpecialDates.querySelectorAll('holiday'),
+        		oHolidayArray = new Array(),
+        		oDaysArray = new Array();
+        	oHolidays.forEach(element => {
+        		oHolidayArray[element.id - 1] = element.getAttribute('title');
+			});
+	        var oDays = oSpecialDates.querySelectorAll('day');
+        	oDays.forEach(element => {
+        		const day = {
+        			date: element.getAttribute('d'),
+        			type: element.getAttribute('t'),
+        			name: +element.getAttribute('h')
+        		}
+        		oDaysArray.push(day);
+			});	        
+    		var oCal = this.byId("calendar"),
 				oLeg = this.byId("legend"),
-				bPressed = oEvent.getParameter("pressed"),
-			    startMonth = oCal.getStartDate().getMonth(),
-				oRefDate;
-			month = data.oData.months;
-			if (bPressed) {
-				oRefDate = new Date();
-				month.forEach(element => {
-					var lastName;
-					element.days.forEach(day => {
-						oRefDate.setMonth(element.month-1, day.date);
-						
-						
-						if(day.name !== "Дополнительный выходной"){
-							oCal.addSpecialDate(new DateTypeRange({
-								startDate : new Date(oRefDate),
-								type : "Type01",
-								tooltip : day.name
-							}));							
-						}
-						else{
-							oCal.addSpecialDate(new DateTypeRange({
-								startDate : new Date(oRefDate),
-								type : "Type02",
-								tooltip : day.name
-							}));								
-						}
-						if(day.name !== "Дополнительный выходной"){
-							if(lastName !== day.name){
-								
-							}
-						}
-						lastName = day.name;
-					});
-				});
-				oLeg.addItem(new CalendarLegendItem({
-					text : "Государственные праздники"
-				}));
-				oLeg.addItem(new CalendarLegendItem({
-					text : "Дополнительные выходные"
-				}));	
-			} else {
-				oCal.destroySpecialDates();
-				oLeg.destroyItems();
-			}
-		}
+				oRefDate;			
+        	oDaysArray.forEach((day)=>{
+        		oRefDate = new Date();
+        		var date = day.date.split('.');
+        		oRefDate.setMonth(+date[0]-1, +date[1]);
+				oCal.addSpecialDate(new DateTypeRange({
+					startDate : new Date(oRefDate),
+					type : "Type0"+day.type,
+					tooltip : day.name ? oHolidayArray[day.name-1] : null
+				}));	        		
+        	})
+			oLeg.addItem(new CalendarLegendItem({
+				text : "Государственные праздники"
+			}));
+			oLeg.addItem(new CalendarLegendItem({
+				text : "Рабочий сокращенный"
+			}));	        	 
+    	},
 	});
-
 });
